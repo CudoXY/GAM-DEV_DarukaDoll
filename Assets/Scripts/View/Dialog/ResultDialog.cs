7a08e8b68ec8aff592879406a1ec9e66713167ba
CudoXY <gelo103097@yahoo.com>
2017-10-10 18:21:09 +0800

    Implemented Result Screen

---
+++ Assets/Scripts/View/Dialog/ResultDialog.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Base popup inherited by other popups
+/// BY: NeilDG
+/// </summary>
+public class ResultDialog : View
+{
+    [SerializeField]
+    private Button btnPlayAgain;
+
+    [SerializeField]
+    private Text lblCorrect;
+
+    [SerializeField]
+    private Text lblWrong;
+
+    public void btnPlayAgain_Click()
+    {
+        LoadManager.Instance.LoadScene(SceneNames.PLAYING_SCENE);
+    }
+
+    public void SetResult(int correct, int wrong)
+    {
+        lblCorrect.text = correct + "";
+        lblWrong.text = wrong + "";
+    }
+}
